0b23aa4cc4927d6b0e249afc46e2101f1b562e64
5910110296 <5910110296@psu.ac.th>
2018-09-17 11:35:01 +0700

    Components & Props (finished)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
-const word = "Hello";
+import WordCard from './WordCard';
 class App extends Component {
   render() {
     return (
         <div>
-         {
-            Array.from(word).map((c , i) => <CharacterCard value ={c} key = {i}/>)
-         }
+          <WordCard value="hello"/>
         </div>
       );
   }
